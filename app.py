3cf41488307d67995520080fd63037fbd69ccb92
Rafael Alvarez <cafepraestudar@gmail.com>
2025-12-16 11:24:59 -0300

    feat: Implementação da função adicionar tarefa.

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 tarefas = []
 
-# Menu Principal
-
+# Lógica da nova tarefa
+def adicionar_tarefa():
+    nova_tarefa = input("Digite a descrição da nova tarefa: ")
+    
+    # Verificação da Lógica
+    if nova_tarefa.strip():
+        tarefas.append(nova_tarefa.strip())
+        print(f"\nTarefa '{nova_tarefa.strip()}' adicionada com sucesso!")
+    else:
+        print("\nPreencha a descrição da tarefa, por favor.")
+    
+# Exibir Menu Principal
 def exibir_menu():
     print("\n--- Gerenciador de Tarefas ---")
     print("1. Adicionar Tarefa")
@@ -10,13 +20,12 @@ def exibir_menu():
     return input("Escolha uma opção: ")
 
 # Lógica do Menu Principal
-
 def opcao_escolhida():
     while True:
         opcao = exibir_menu()
 
         if opcao == '1':
-            print("Você escolheu Adicionar Tarefa.")
+            adicionar_tarefa()
 
         elif opcao == '2':
             print("Você escolheu Listar Tarefas.")
